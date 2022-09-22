5df5b8cbd243a679697ef62628bec757be9c20d3
henoth <henocik997@gmail.com>
2022-09-22 13:22:38 +0200

    feat: try to update file but too much for me

--- src/person.controller.ts
+++ src/person.controller.ts
@@ -1,6 +1,6 @@
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
 import { PersonDto } from './domain/dto/person.dto';
 import { Person } from './domain/person';
 import { Person as PersonModel, PersonDocument } from './schema/person.schema';
@@ -21,6 +21,25 @@ export class PersonController {
     return this.personModel.findById(params.id).exec();
   }
 
+  @Delete(":id")
+  @HttpCode(200)
+  DeleteById(@Param() params: {id: string}): Promise<PersonModel>  {
+    return this.personModel.findByIdAndRemove(params.id).exec();
+  }
+
+  @Post(":id")
+  @HttpCode(200)
+  UpdateById(@Param() params: {id: string}): Promise<PersonModel> {
+    return this.personModel.findById(params.id).update(
+      {
+        isTrustedByKarakan: false,
+      },
+      {
+        itTrusedByKarakan: true,
+      }
+      ).exec();
+  }
+
   @Post()
   @HttpCode(201)
   createPerson(@Body() personDto: PersonDto): Promise<PersonModel> {
@@ -28,7 +47,4 @@ export class PersonController {
 
     return createdPerson.save();
   }
-
-  
-
 }
